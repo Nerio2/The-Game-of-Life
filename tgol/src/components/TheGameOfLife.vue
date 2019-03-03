54f1fa277c440f62608e74ede6452f95d25b5fcb
Patryk Kozarski <Patryknerio2@gmail.com>
2019-03-03 10:29:28 +0100

    vue init

---
+++ tgol/src/components/TheGameOfLife.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>The Game Of Life</h2>
+    </div>
+</template>
+
+<script>
+    import TheGameOfLifeUnit from './TheGameOfLifeUnit.vue'
+    export default {
+        name: "TheGameOfLifeUnit",
+        components:{
+            TheGameOfLifeUnit
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ tgol/src/components/TheGameOfLifeUnit.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "TheGameOfLifeUnit"
+    }
+</script>
+
+<style scoped>
+
+</style>
