ecda086ddc2740c1a69f38e0171f17f36edde6fd
Patryk Kozarski <Patryknerio2@gmail.com>
2019-03-04 21:38:59 +0100

    need fix

--- tgol/src/components/TheGameOfLife.vue
+++ tgol/src/components/TheGameOfLife.vue
@@ -1,10 +1,9 @@
 <template>
     <div id="TheGameOfLife">
         <h2>The Game Of Life</h2>
-        <section v-bind:key="n" class="row" v-for="n in rows">
-            <TheGameOfLifeUnit v-bind:style="{'width':unit_width+'%'}" v-bind:key="m" v-for="m in cols"></TheGameOfLifeUnit>
+        <section v-bind:key="y" class="row" v-for="y in rows">
+            <TheGameOfLifeUnit v-bind:style="{'width':unit_width+'%'}" v-bind:key="x" v-for="x in cols" v-on:click="cosik()" :alive="status[y-1][x-1]"></TheGameOfLifeUnit>
         </section>
-
     </div>
 </template>
 
@@ -22,7 +21,31 @@
         },
         data() {
             return {
-                unit_width: (100 / this.cols)
+                unit_width: (100 / this.cols),
+                status: [],
+                prevStatus: [],
+                y: 0,
+                x: 0
+            }
+        },
+        methods: {
+            move(x,y) {
+                status[y-1][x-1]=!status[y-1][x-1];
+                console.log(status[y-1][x-1]);
+            },
+            cosik(){
+                console.log("dziala");
+            }
+        },
+        created() {
+            for (let y = 0; y <= this.rows; y++) {
+                this.status.push([]);
+                for (let x = 0; x <= this.cols; x++) {
+                    this.status[y].push(false);
+                }
+            }
+            for (let i = 0; i < this.rows; i++) {
+                this.status[i][i]=true;
             }
         }
     }
--- tgol/src/components/TheGameOfLifeUnit.vue
+++ tgol/src/components/TheGameOfLifeUnit.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-on:click="alive=!alive" id="TheGameOfLifeUnit" :class="getClass()"></div>
+    <div id="TheGameOfLifeUnit" :class="getClass()"></div>
 
 </template>
 
@@ -7,38 +7,39 @@
     export default {
         name: "TheGameOfLifeUnit",
         props: {
+            alive: Boolean
         },
         computed: {
             style() {
                 return 'width:' + this.width + '%}';
             }
         },
-        methods:{
-            getClass(){
-                if(this.alive) return "life";
+        methods: {
+            getClass() {
+                if (this.alive) return "life";
                 else return "dead";
             }
         },
-        data(){
-            return{
-                alive: Boolean
-            }
+        data() {
+            return {}
         },
         created() {
-            this.alive=false;
+
         }
     }
 </script>
 <style scoped>
     #TheGameOfLifeUnit {
         float: left;
-        height:20px;
-        box-shadow: inset 0px 0px 1px 0px rgba(0,0,0,0.75);
+        height: 20px;
+        box-shadow: inset 0px 0px 1px 0px rgba(0, 0, 0, 0.75);
     }
-    .life{
+
+    .life {
         background-color: seagreen;
     }
-    .dead{
-        background-color: darkgray ;
+
+    .dead {
+        background-color: darkgray;
     }
 </style>
